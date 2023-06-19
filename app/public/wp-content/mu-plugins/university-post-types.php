eeff0e7dd8d5268b4e9df264b1540a289b85f2b6
Alan Avery <delayedaa@gmail.com>
2023-06-19 16:34:08 -0500

    40

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -19,7 +19,7 @@ function university_post_types() {
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
-  
+
   // Program Post Type ————————————————————
   register_post_type('program', array(
     'supports' => array('title', 'editor'),
@@ -37,6 +37,21 @@ function university_post_types() {
       'singular_name' => 'Program'
     ),
     'menu_icon' => 'dashicons-awards'
+  ));
+
+  // Professor Post Type ————————————————————
+  register_post_type('professor', array(
+    'supports' => array('title', 'editor'),
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
   ));
 }
 
